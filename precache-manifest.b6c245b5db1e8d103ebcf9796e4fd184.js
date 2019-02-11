ccae694c9763285e8e9e6eb2c4d15c57705b665c
temir <temir230197@mail.ru>
2019-02-11 19:13:44 +0600

    course exchange commit 1

---
+++ precache-manifest.b6c245b5db1e8d103ebcf9796e4fd184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04eaaeb7636c2d9b7ded8f3a3c2941c9",
+    "url": "/static/media/flags.04eaaeb7.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15cae82447e7cb5877d8",
+    "url": "/static/js/main.15cae824.chunk.js"
+  },
+  {
+    "revision": "2c422f515e3160dc3d70",
+    "url": "/static/js/1.2c422f51.chunk.js"
+  },
+  {
+    "revision": "15cae82447e7cb5877d8",
+    "url": "/static/css/main.1e69028c.chunk.css"
+  },
+  {
+    "revision": "00b24d1c163e56651aa9c4676f0ceb6b",
+    "url": "/index.html"
+  }
+];
